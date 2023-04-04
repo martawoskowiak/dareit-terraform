f79490046fdedb05297e93c5d171f6a93608f9c8
martawoskowiak <marta.woskowiak@gmail.com>
2023-04-04 22:22:51 +0200

    Update main.tf
    Updated resource's arguments

--- main.tf
+++ main.tf
@@ -2,4 +2,6 @@ resource "google_storage_bucket" "bucket-cicd-terraform" {
   name          = "bucket-cicd-terraform"
   location      = "EU"
   force_destroy = true
+
+  uniform_bucket_level_access = true
 }
