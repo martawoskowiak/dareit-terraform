22b06d74b13f0cbdd0e9ccdf1f097f26b6eb9a22
martawoskowiak <125698538+martawoskowiak@users.noreply.github.com>
2023-04-04 22:37:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-resource "google_storage_bucket" "bucket-cicd-terraform" {
-  name          = "bucket-cicd-terraform"
+resource "google_storage_bucket" "bucket-cicd" {
+  name          = "marta-ci-bucket"
   location      = "EU"
   force_destroy = true
-  
+
+  uniform_bucket_level_access = true
 }
