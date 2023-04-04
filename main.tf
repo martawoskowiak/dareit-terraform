f8db9d9d8bcba68cd51694006de83bfcc22f3d5d
martawoskowiak <marta.woskowiak@gmail.com>
2023-04-04 21:52:00 +0200

    Fix: changed bucket name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_storage_bucket" "terraform-state-file-marta" {
-  name          = "terraform-state-file-marta"
+resource "google_storage_bucket" "bucket-cicd-terraform" {
+  name          = "bucket-cicd-terraform"
   location      = "EU"
   force_destroy = true
 }
