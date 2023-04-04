179e6c3db3fa8314a8b20346321e51015cef27b6
martawoskowiak <marta.woskowiak@gmail.com>
2023-04-04 21:50:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-resource "google_compute_instance" "dareit-vm-ci" {
-  name         = "dareit-vm-tf-ci"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-
-  tags = ["dareit"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        managed_by_terraform = "true"
-      }
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-}
-
 resource "google_storage_bucket" "terraform-state-file-marta" {
   name          = "terraform-state-file-marta"
   location      = "EU"
