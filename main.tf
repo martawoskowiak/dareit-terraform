d257ce4bf3b70e5507d863834e7d43e7fe334755
martawoskowiak <marta.woskowiak@gmail.com>
2023-04-04 21:40:13 +0200

    Edited GitHub Actions workflow file with pull request

--- main.tf
+++ main.tf
@@ -23,8 +23,8 @@ resource "google_compute_instance" "dareit-vm-ci" {
   }
 }
 
-resource "google_storage_bucket" "terraform-state-file-marta" {
-  name          = "terraform-state-file-marta"
+resource "google_storage_bucket" "bucket-cicd-terraform" {
+  name          = "bucket-cicd-terraform"
   location      = "EU"
   force_destroy = true
 }
