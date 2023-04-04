d30c3bccd93ce54e15c2287716ebd550374bb5eb
martawoskowiak <marta.woskowiak@gmail.com>
2023-04-04 21:14:20 +0200

    update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,9 +3,3 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-c"
 }
-
-provider "google-beta" {
-  project = "seismic-vista-378120"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
